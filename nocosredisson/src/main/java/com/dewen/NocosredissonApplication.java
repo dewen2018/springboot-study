aa5572622ee140e22f565dca7fac1ab51ecc544a
dewen <863572313@qq.com>
2021-07-05 16:23:53 +0800

    整合springboot nacos

--- nocosredisson/src/main/java/com/dewen/NocosredissonApplication.java
+++ nocosredisson/src/main/java/com/dewen/NocosredissonApplication.java
@@ -1,13 +1,12 @@
 package com.dewen;
 
-import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@EnableNacosConfig
-//@NacosPropertySource(name = "dewen", dataId = "n0001",groupId = "group0001",autoRefreshed = true)
+@NacosPropertySource(dataId = "n0001", groupId = "n0001-group", autoRefreshed = true)
 public class NocosredissonApplication {
 
     public static void main(String[] args) {
